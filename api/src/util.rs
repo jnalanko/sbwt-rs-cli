e997127e1a2c92d5a667d55f596121efe2cc1d7f
Jarno N. Alanko <alanko.jarno@gmail.com>
2025-12-05 12:30:55 +0200

    Test out-of-bounds

--- api/src/util.rs
+++ api/src/util.rs
@@ -446,7 +446,7 @@ mod tests {
     fn for_each_one_bit_empty() {
         let words = [u64::MAX, u64::MAX, u64::MAX];
 
-        for s in 0..64*3 {
+        for s in 0..=64*3 {
             check_for_each_one_bit(&words, s..s);
         }
     }
