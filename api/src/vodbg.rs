89e77dfe7f7e47d193e3d3b0d9042182a75a0f56
Jarno Niklas Alanko <alanko.jarno@gmail.com>
2026-08-01 09:58:46 +0300

    WIP make merge compatible with build refactor

--- api/src/vodbg.rs
+++ api/src/vodbg.rs
@@ -651,7 +651,7 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::{BitPackedKmerSortingMem, LcsArray, SbwtIndexBuilder, SubsetMatrix};
+    use crate::{LcsArray, SbwtIndexBuilder, SubsetMatrix, init_bitpacked_kmer_sorting_disk_from_vecs, init_bitpacked_kmer_sorting_from_vecs};
     use crate::dbg::Dbg;
     use pnsv::PnsvTuned;
 
@@ -679,11 +679,11 @@ mod tests {
         seqs.sort();
         seqs.dedup();
 
-        let (sbwt, lcs) = SbwtIndexBuilder::<BitPackedKmerSortingMem>::new()
+        let (sbwt, lcs) = SbwtIndexBuilder::new(init_bitpacked_kmer_sorting_from_vecs(&seqs))
             .k(max_k).build_lcs(true)
             .add_all_dummy_paths(true)
             .build_select_support(true)
-            .run_from_vecs(&seqs);
+            .run();
         let lcs = lcs.unwrap();
         let pnsv_tuned = PnsvTuned::new_default(&sbwt, &lcs, max_k);
 
@@ -745,10 +745,10 @@ mod tests {
         let mut graphs = Vec::with_capacity(max_k);
 
         for i in MIN_K..=max_k {
-            let (sbwt, lcs) = SbwtIndexBuilder::<BitPackedKmerSortingMem>::new()
+            let (sbwt, lcs) = SbwtIndexBuilder::new(init_bitpacked_kmer_sorting_disk_from_vecs(&seqs))
                 .k(i).build_lcs(true)
                 .build_select_support(true)
-                .run_from_vecs(seqs.as_slice());
+                .run();
             sbwt_indices.push((sbwt, lcs));
         }
 
@@ -898,10 +898,10 @@ mod tests {
     fn smaller_values_of_k() {
         // All possible 2-mers.
         let seqs = vec![b"AACCGGTTAGCTGATCA".to_vec()];
-        let (sbwt, lcs) = SbwtIndexBuilder::<BitPackedKmerSortingMem>::new()
+        let (sbwt, lcs) = SbwtIndexBuilder::new(init_bitpacked_kmer_sorting_from_vecs(&seqs))
             .k(3).build_lcs(true)
             .build_select_support(true)
-            .run_from_vecs(seqs.as_slice());
+            .run();
         let lcs = lcs.unwrap();
 
         let pnsv_tuned = PnsvTuned::new_default(&sbwt, &lcs, sbwt.k());
