25f33b7a2546aba64b12d8d37157e8eed93e503e
Jarno N. Alanko <alanko.jarno@gmail.com>
2026-09-21 13:24:03 +0300

    Make modules private

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -202,9 +202,9 @@ pub use streaming_index::{StreamingIndex, LcsArray, MatchingStatisticsIterator, 
 mod subsetseq;
 pub use subsetseq::*;
 
-pub mod pred8_vs1;
-pub mod pino;
-pub mod quad_rank;
+mod pred8_vs1;
+mod pino;
+mod quad_rank;
 mod subset_correction_sets;
 pub use subset_correction_sets::*;
 
